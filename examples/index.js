61b37d82795ea76177bd372a31b1be8502f2a02b
donotlb <donotlb@gmail.com>
2022-02-21 22:26:03 +0800

    chore: add examples

---
+++ examples/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable func-names */
+import DIDMotif from '../src/modules/motif';
+
+(function () {
+  const canvas = document.getElementById('example-1');
+  const motif = new DIDMotif({ did: 'z35n6X6rDp8rWWCGSiXZgvd42bixdLULmX8oX', canvas });
+  motif.render();
+})();
+
+(function () {
+  const canvas = document.getElementById('example-2');
+  const motif = new DIDMotif({ did: 'z35n6X6rDp8rWWCGSiXZgvd42bixdLULmX8oX', canvas });
+  motif.animate();
+})();
+
+(function () {
+  const img = document.getElementById('example-3');
+  img.src = DIDMotif.toDataURL({ did: 'z35n6X6rDp8rWWCGSiXZgvd42bixdLULmX8oX' })
+})();
