a5f0170d34f0972d993b9b77edac9eda535f5ce6
donotlb <donotlb@gmail.com>
2022-02-22 13:07:46 +0800

    build: tweak build formats

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       name: 'DIDMotif',
+      formats: ['es'],
       fileName: format => `did-motif.${format}.js`,
     },
   },
